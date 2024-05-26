4a5ea13717e705a39a845e565287febc65735242
elikapiiro <100676931+elikapiiro@users.noreply.github.com>
2024-05-26 13:21:02 +0300

    Update dataErasure.ts

--- routes/dataErasure.ts
+++ routes/dataErasure.ts
@@ -93,4 +93,46 @@ router.post('/', async (req: Request<Record<string, unknown>, Record<string, unk
   }
 })
 
+router.post('/someRiskyApi', async (req: Request<Record<string, unknown>, Record<string, unknown>, DataErasureRequestParams>, res: Response, next: NextFunction): Promise<void> => {
+  const loggedInUser = insecurity.authenticatedUsers.get(req.cookies.token)
+  if (!loggedInUser) {
+    next(new Error('Blocked illegal activity by ' + req.socket.remoteAddress))
+    return
+  }
+
+  try {
+    await PrivacyRequestModel.create({
+      UserId: loggedInUser.data.id,
+      deletionRequested: true
+    })
+
+    res.clearCookie('token')
+    if (req.body.layout) {
+      const filePath: string = path.resolve(req.body.layout).toLowerCase()
+      const isForbiddenFile: boolean = (filePath.includes('ftp') || filePath.includes('ctf.key') || filePath.includes('encryptionkeys'))
+      if (!isForbiddenFile) {
+        res.render('dataErasureResult', {
+          ...req.body
+        }, (error, html) => {
+          if (!html || error) {
+            next(new Error(error.message))
+          } else {
+            const sendlfrResponse: string = html.slice(0, 100) + '......'
+            res.send(sendlfrResponse)
+            challengeUtils.solveIf(challenges.lfrChallenge, () => { return true })
+          }
+        })
+      } else {
+        next(new Error('File access not allowed'))
+      }
+    } else {
+      res.render('dataErasureResult', {
+        ...req.body
+      })
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
 export default router
